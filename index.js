12c269e9820f805db056d9817990917f50e9dd50
MaxMates <72421585+MaxMates@users.noreply.github.com>
2021-03-21 21:01:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const { token, prefix } = require('./config.json')
+const MenuDocsClient = require('./Structures/MenuDocsClient');
+const config = require('../config.json');
 
-client.on('ready', () => {
-    console.log('Botty is online')
-    client.user.setActivity('Hello, use ' + prefix + 'help to see my commands')
-})
-
-client.on('message', message => {
-    if (message.content === prefix + 'ping') {
-        message.channel.send(`My latency is: ${client.ws.ping}ms`)
-    }
-})
-
-client.login(token)
+const client = new MenuDocsClient(config);
+client.start();
